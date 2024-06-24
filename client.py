a5a4d48e13a5a6c094f199287568da7b52e99b38
Hidayah Ramadlana <hidayahweb@gmail.com>
2024-06-24 16:47:49 +0700

    Fix

--- client.py
+++ client.py
@@ -1,33 +1,48 @@
 import socket
-def udp_server():
+import string
+import random
+
+def generate_random_data(size):
+    char = string.ascii_letters + string.digits
+    return "".join(random.choice(char) for _ in range(size))
+
+def udp_client():
     server_ip = "172.16.81.100"
-    server_port=11000
-    client_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_port = 11000
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sequence_number = 0
     udp_dropped = 0
-    while (sequence_number < 1000):
-        message = f"UDP Datagram ke-{sequence_number}".encode()
+    while sequence_number < 1000:
+        voice_data_dummy = generate_random_data(8*1024)  # generate 8KB data
+        message = f"UDP Datagram ke-{sequence_number} - {voice_data_dummy}"
+        encoded_message = message.encode()
 
-        client_socket.sendto(message,(server_ip, server_port))
+        #print(f"Sending message: {message}")
+        client_socket.sendto(encoded_message, (server_ip, server_port))
         client_socket.settimeout(1)
 
         # Wait for ACK
         try:
-            acknowledgement, _ = client_socket.recvfrom(1024)
-            if acknowledgement.decode() == f"Acknowledgement {message.decode()}":
-                print(f"{acknowledgement.decode()}")
+            acknowledgement, _ = client_socket.recvfrom(9000)
+            received_ack = acknowledgement.decode()
+            expected_ack = f"Acknowledgement {message}"
+            
+            if received_ack == expected_ack:
+               print(f"Received correct ACK: {received_ack} \n\n")
             else:
-                print("invalid ack")
+                print(f"Invalid ACK")
+            
             sequence_number += 1
         except socket.timeout:
             udp_dropped += 1
             print(f"!!!!!!!!!!!!!!! UDP Datagram ke-{sequence_number} Gagal diterima peer !!!!!!!!!!")
             sequence_number += 1
             continue
-    print("total datagram drop: ",udp_dropped)
-    print("total datagram dikirim: ", sequence_number)
+    print("Total datagram drop:", udp_dropped)
+    print("Total datagram dikirim:", sequence_number)
+
 def main():
-    udp_server()
+    udp_client()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -2,19 +2,21 @@ import socket
 
 def udp_server():
     server_ip = "172.16.81.100"
-    server_port=11000
-    server_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((server_ip,server_port))
-    print("UDP server listening on port 11000")
-    while True:
-        data, client_address = server_socket.recvfrom(1024)
-        print(f"received message from {client_address}: {data.decode()}")
+    server_port = 11000
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((server_ip, server_port))
 
-        #send ack
-        acknowledgement = f"Acknowledgement {data.decode()}".encode()
-        server_socket.sendto(acknowledgement, client_address)
-def main():
-    udp_server()
+    print(f"Server listening on {server_ip}:{server_port}")
+
+    while True:
+        message, client_address = server_socket.recvfrom(9000)
+        received_message = message.decode()
+        print(f"Received message from {client_address}: {received_message}")
+        
+        # Construct the acknowledgment message
+        ack_message = f"Acknowledgement {received_message}"
+        print(f"Sending ACK to {client_address}: {ack_message}\n\n")
+        server_socket.sendto(ack_message.encode(), client_address)
 
 if __name__ == "__main__":
-    main()
+    udp_server()
